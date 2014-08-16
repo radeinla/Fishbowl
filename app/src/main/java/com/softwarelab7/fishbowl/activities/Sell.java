73fda578a656bb267d29fa36d9563e747f3d29b3
Reginald Eli Deinla <radeinla@gmail.com>
2014-08-17 01:17:25 +0800

    Background processing of get address request.

--- app/src/main/java/com/softwarelab7/fishbowl/activities/Sell.java
+++ app/src/main/java/com/softwarelab7/fishbowl/activities/Sell.java
@@ -1,21 +1,49 @@
 package com.softwarelab7.fishbowl.activities;
 
 import android.app.Activity;
+import android.content.IntentSender;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationRequest;
 import com.softwarelab7.fishbowl.R;
 import com.softwarelab7.fishbowl.models.Sale;
 import com.softwarelab7.fishbowl.models.Session;
 import com.softwarelab7.fishbowl.sqlite.DbHelper;
 
+import java.io.IOException;
 import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 
-public class Sell extends Activity {
+public class Sell extends Activity implements
+        GooglePlayServicesClient.ConnectionCallbacks,
+        GooglePlayServicesClient.OnConnectionFailedListener,
+        com.google.android.gms.location.LocationListener {
+
+    private final static float LOCATION_CHANGE_THRESHOLD = 20.0f;
+
+    private final static int
+            CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+
+    private static final String TAG = "Sell";
+
+    private LocationClient mLocationClient;
 
     DbHelper dbHelper;
     Session activeSession;
@@ -23,12 +51,14 @@ public class Sell extends Activity {
     String location = "UP Diliman";
     Double lat = 14.6549;
     Double lon = 121.0645;
+    AsyncTask<Location,Void,Address> lastAsyncAddressTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        dbHelper = new DbHelper(getApplicationContext());
         setContentView(R.layout.activity_sell);
+        dbHelper = new DbHelper(getApplicationContext());
+        mLocationClient = new LocationClient(this, this, this);
         initializeContentView();
         findViewById(R.id.sale).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -89,6 +119,134 @@ public class Sell extends Activity {
 
     private void increaseSoldCount() {
         setSold(sold+1);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        if (GooglePlayServicesUtil.isGooglePlayServicesAvailable(this) == ConnectionResult.SUCCESS) {
+            LocationRequest locationRequest = LocationRequest.create()
+                    .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                    .setSmallestDisplacement(LOCATION_CHANGE_THRESHOLD)
+                    .setInterval(TimeUnit.MINUTES.toMillis(10));
+            Log.d(TAG, "Expiration! " + locationRequest.getExpirationTime());
+            mLocationClient.requestLocationUpdates(locationRequest, this);
+            Location currentLocation = mLocationClient.getLastLocation();
+            if (currentLocation != null) {
+                this.onLocationChanged(currentLocation);
+            }
+        } else {
+            Toast.makeText(this, "GPS not available. Please install GooglePlayServices and enable GPS.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onDisconnected() {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Toast.makeText(this, "GPS not available. Please install GooglePlayServices and enable GPS.", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "Was not able to connect to google play services");
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(
+                        this,
+                        CONNECTION_FAILURE_RESOLUTION_REQUEST);
+                /*
+                 * Thrown if Google Play services canceled the original
+                 * PendingIntent
+                 */
+            } catch (IntentSender.SendIntentException e) {
+                // Log the error
+                e.printStackTrace();
+            }
+        } else {
+        }
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mLocationClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        mLocationClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d(TAG, "Location changed!!!!!");
+        if (location.distanceTo(getCurrentLocation()) > LOCATION_CHANGE_THRESHOLD) {
+            lat = location.getLatitude();
+            lon = location.getLongitude();
+            if (lastAsyncAddressTask != null) {
+                lastAsyncAddressTask.cancel(true);
+            }
+            lastAsyncAddressTask = new AsyncTask<Location,Void,Address>() {
+                @Override
+                protected void onPostExecute(Address address) {
+                    setLocation(getLocationFromAddress(address));
+                }
+
+                @Override
+                protected Address doInBackground(Location... params) {
+                    return getAddress(params[0]);
+                }
+            };
+            lastAsyncAddressTask.execute(location);
+        }
+    }
+
+    private String getLocationFromAddress(Address address) {
+        if (address == null) {
+            return getCoordinateString();
+        } else {
+            if (address.getMaxAddressLineIndex() > -1) {
+                return address.getAddressLine(0);
+            } else {
+                if (address.getLocality() == null) {
+                    return address.getCountryCode();
+                } else {
+                    return address.getLocality();
+                }
+            }
+        }
+    }
+
+    private String getCoordinateString() {
+        return "("+lat+","+lon+")";
+    }
+
+    private Location getCurrentLocation() {
+        Location location = new Location(Sell.class.getSimpleName());
+        location.setLatitude(lat);
+        location.setLatitude(lon);
+        return location;
+    }
+
+    private Address getAddress(Location location) {
+        try {
+            Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+            List<Address> addressList = geocoder.getFromLocation(
+                    location.getLatitude(), location.getLongitude(), 1);
+            if (addressList != null && !addressList.isEmpty()) {
+                Address address = addressList.get(0);
+                System.out.println("Locality: " + address.getLocality());
+                System.out.println("Country: " + address.getCountryCode());
+                return address;
+            } else {
+                return null;
+            }
+        } catch (IOException ioException) {
+            Log.d(TAG, "Received ioException from geocoding: " + ioException.getMessage(), ioException);
+            return null;
+        }
     }
 
 }
